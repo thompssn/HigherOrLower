8d26a8c9656b168d5c336605a91164100cf1b217
thompssn <151627110+thompssn@users.noreply.github.com>
2023-11-21 05:40:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import random
 
+def user_mode():
+  ###
+
+def computer_mode():
+  ###
+
+mode = input("Choose the mode (user/computer): ").lower()
+
+if mode == "user":
+    user_mode()
+elif mode == "computer":
+    computer_mode()
+else:
+    print("Invalid mode. Please enter 'user' or 'computer'.")
