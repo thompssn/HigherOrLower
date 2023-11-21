4cc14019f2ec31e1eb161383a6e1128b65c68f83
Gerald Thompson <151627110+thompssn@users.noreply.github.com>
2023-11-21 09:51:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
 import random
+import time
 
 def user_mode():
   ###
 
 def computer_mode():
-  ###
+    print("Welcome to computer mode! The computer will guess your number.")
+    
+    # get the range and the secret number from the user
+    lower_limit = int(input("Enter the lower limit of the range: "))
+    upper_limit = int(input("Enter the upper limit of the range: "))
+    secret_number = int(input(f"Enter your secret number between {lower_limit} and {upper_limit}: "))
+    
+    # initialize variables
+    computer_guess = ''
+    guesses_count = 0
+    
+    # computer guesses until correct
+    while computer_guess != secret_number:
+        computer_guess = random.randint(lower_limit, upper_limit)
+        guesses_count += 1
+        
+        print(f"Computer's guess: {computer_guess}")
+        
+        if computer_guess < secret_number:
+            print("Too low! Try again.")
+            lower_limit = computer_guess + 1  # adjust limit for the next guess
+        elif computer_guess > secret_number:
+            print("Too high! Try again.")
+            upper_limit = computer_guess - 1  # adjust limit for the next guess
+        
+        time.sleep(1)  # adds a delay between computer guesses
+    
+    # win
+    print(f"Computer guessed the correct number {secret_number} in {guesses_count} guesses.")
 
 mode = input("Choose the mode (user/computer): ").lower()
 
