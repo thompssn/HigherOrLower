2eee6d128140581f7e7c04be4c2b5eff8abab575
Gerald Thompson <151627110+thompssn@users.noreply.github.com>
2023-11-21 14:14:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,26 +4,30 @@ import time
 def user_mode():
   print("Welcome to user mode! You will guess a randomized number.")
 
+  # get the range from user
   lower_limit=int(input("Enter the lower limit of the range: "))
   upper_limit=int(input("Enter the upper limit of the range: "))
 
+  # generate a secret number in the range
   secret_number=random.randint(lower_limit,upper_limit)
 
+  # initialize variables
   user_guess=""
   guesses_count=0
 
-  while user_guesses != secret_number:
-    user_guess=int(input(f"Guess the number between {lower_limit}  and {upper_limit}: "))
+  # user guesses until correct
+  while user_guess != secret_number:
+    user_guess=int(input(f"Guess the number between {lower_limit} and {upper_limit}: "))
     guesses_count += 1
 
     if user_guess < secret_number:
-      print("Too Low! Try Again.")
+      print("Too low! Try again.")
     elif user_guess > secret_number:
-      print("Too High! Try Again.")
-  print( f"Congratulations! You Guessed the correct number {secret_number} in {guesses_count} guesses.")
-  
-  
+      print("Too high! Try again.")
 
+  # win
+  print( f"Congratulations! You guessed the correct number {secret_number} in {guesses_count} guesses.")
+  
 def computer_mode():
     print("Welcome to computer mode! The computer will guess your number.")  
     
@@ -55,6 +59,18 @@ def computer_mode():
     # win
     print(f"Computer guessed the correct number {secret_number} in {guesses_count} guesses.")
 
+title = """
+  _    _ _       _                ____       _                          ___  
+ | |  | (_)     | |              / __ \     | |                        |__ \ 
+ | |__| |_  __ _| |__   ___ _ __| |  | |_ __| |     _____      _____ _ __ ) |
+ |  __  | |/ _` | '_ \ / _ \ '__| |  | | '__| |    / _ \ \ /\ / / _ \ '__/ / 
+ | |  | | | (_| | | | |  __/ |  | |__| | |  | |___| (_) \ V  V /  __/ | |_|  
+ |_|  |_|_|\__, |_| |_|\___|_|   \____/|_|  |______\___/ \_/\_/ \___|_| (_)  
+            __/ |                                                            
+           |___/                                                             
+"""
+print(title)
+
 mode = input("Choose the mode (user/computer): ").lower()
 
 if mode == "user":
@@ -63,3 +79,5 @@ elif mode == "computer":
     computer_mode()
 else:
     print("Invalid mode. Please enter 'user' or 'computer'.")
+
+input("Press return to exit.") # the console won't close until the user is done reading
